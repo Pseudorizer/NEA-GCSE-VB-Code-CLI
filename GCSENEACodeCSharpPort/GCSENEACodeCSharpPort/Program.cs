4e73dc57db02028d93c66cff9aa156f8afea538b
MiniCheese26 <kaleb.medhurst@gmail.com>
2018-02-15 18:21:47 +0000

    Added initial commit for the C# port.
    I would not use this code at all. I'm still learning the structure of C# and how to use it properly and effectively so the code will probably quite inefficient for a while.

---
+++ GCSENEACodeCSharpPort/GCSENEACodeCSharpPort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCSENEACodeCSharpPort
+{
+    class Start
+    {
+        static void Main()
+        {
+            string space = new string(' ', 1);
+            int e = 0;
+
+            do {
+                Console.WriteLine("Do you have an account? Yes or No?");
+                string name = Console.ReadLine();
+                name.ToLower();
+
+                if (name == "yes")
+                {
+                    Console.WriteLine("yes test");
+                }
+                else if (name == "no")
+                {
+                    Console.WriteLine("No test");
+                }
+                else
+                {
+                    Console.WriteLine("Error incorrect response");
+                    Console.WriteLine(space);
+                    e++;
+                }
+            } while (e == 1);
+
+         Console.ReadKey();
+        }
+        public void UserInfo()
+        {
+
+        }
+    }
+    class FileRW
+    {
+
+    }
+}
