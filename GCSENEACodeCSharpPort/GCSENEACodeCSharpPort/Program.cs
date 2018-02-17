691d70b661b857dbd528e416bf34820067689b79
MiniCheese26 <kaleb.medhurst@gmail.com>
2018-02-17 16:51:12 +0000

    Started work on creating files/folders

--- GCSENEACodeCSharpPort/GCSENEACodeCSharpPort/Program.cs
+++ GCSENEACodeCSharpPort/GCSENEACodeCSharpPort/Program.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace GCSENEACodeCSharpPort
 {
     class Start
     {
-        static List<string> UserInfo()
+        public static List<string> UserInfo()
         {
             string[] writeIndex = new string[4] { "name", "password", "age", "year group" };
             List<string> savedValues = new List<string>();
@@ -34,26 +35,80 @@ namespace GCSENEACodeCSharpPort
                 string AccountCheck = Console.ReadLine();
                 AccountCheck.ToLower();
 
-                if (AccountCheck == "yes")
+                Console.WriteLine(space);
+
+                switch (AccountCheck)
                 {
-                    
-                }
-                else if (AccountCheck == "no")
-                {
-                    userData = UserInfo();
-                    string t = userData[0];
-                    int j = Convert.ToInt32(userData[1]);
-                    userName = t.Substring(0, 3) + j;
-                }
-                else
-                {
-                    Console.WriteLine("Error incorrect response");
-                    Console.WriteLine(space);
-                    l = true;
+                    case "yes":
+
+                        break;
+                    case "no":
+                        userData = UserInfo();
+                        string t = userData[0];
+                        int j = Convert.ToInt32(userData[2]);
+                        userName = t.Substring(0, 3) + j;
+                        bool test = FileWrite.DirExist(userName);
+                        if (test == false)
+                        {
+                            Console.WriteLine("1");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Error incorrect response");
+                        Console.WriteLine(space);
+                        l = true;
+                        break;
                 }
             } while (l == true);
 
          Console.ReadKey();
+        }
+    }
+
+    class FileWrite: Start
+    {
+        public static bool DirExist(string path)
+        {
+            return (Directory.Exists(path));
+        }
+
+        private void FS(string dir, string userName)
+        {
+            string[] fileNames = new string[5] { "Name", "Password", "Age", "Year Group", "Username" };
+
+            FileStream fileCreate;
+
+            userName = userName + @"\";
+
+            for (int i = 0; i < 5; i++)
+            {
+                string p = fileNames[i] + ".txt";
+                fileCreate = File.Create(dir + userName + p);
+            }
+        }
+
+        public static bool DirCheck(string userName)
+        {
+            string root = Path.GetPathRoot(Directory.GetCurrentDirectory());
+            string dir = (root + @"NeaFiles\");
+
+            bool dirCheck = DirExist(dir);
+
+            if (dirCheck == true)
+            {
+
+            }
+            else if (dirCheck == false)
+            {
+                Directory.CreateDirectory(dir);
+
+                Directory.CreateDirectory(dir + userName);
+               
+            }
+
+            dirCheck = DirExist(dir);
+
+            return dirCheck;
         }
     }
 }
