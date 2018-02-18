722e53f0386cda154f4b75cc310578059accd6b3
MiniCheese26 <kaleb.medhurst@gmail.com>
2018-02-18 17:27:55 +0000

    Split classes into multiple files

--- GCSENEACodeCSharpPort/GCSENEACodeCSharpPort/Program.cs
+++ GCSENEACodeCSharpPort/GCSENEACodeCSharpPort/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace GCSENEACodeCSharpPort
 {
@@ -28,6 +27,7 @@ namespace GCSENEACodeCSharpPort
                 {
                     case "yes":
                         Login.SignIn();
+                        Console.WriteLine("done");
                         break;
                     case "no":
                         userData = UserInfo();
@@ -36,7 +36,7 @@ namespace GCSENEACodeCSharpPort
                         userName = t.Substring(0, 3) + j;
                         userData[4] = userName;
 
-                        FileWrite.MainFW((string[])userData.ToArray());
+                        FileOps.MainFW((string[])userData.ToArray());
                         Login.SignIn();
                         break;
                     default:
@@ -62,119 +62,6 @@ namespace GCSENEACodeCSharpPort
             }
 
             return userData;
-        }
-    }
-
-    class FileWrite : Start
-    {
-        public static string GetUserDir(string inputUserName)
-        {
-            string root = Path.GetPathRoot(Directory.GetCurrentDirectory());
-            string dir = root + @"NeaFiles\" + inputUserName;
-
-            return dir;
-        }
-
-        public static void FileWriter(string dir, string[] userDataArray)
-        {
-            string[] fileNames = new string[5] { "Name", "Password", "Age", "Year Group", "Username" };
-            string userName = userDataArray[4];
-
-            FileStream fileCreate;
-
-            userName = userName + @"\";
-
-            for (int i = 0; i < 5; i++)
-            {
-                string p = fileNames[i] + ".txt";
-                fileCreate = File.Create(dir + userName + p);
-                fileCreate.Close();
-
-                StreamWriter fileStreamWriter = new StreamWriter(dir + userName + p);
-
-                fileStreamWriter.WriteLine(userDataArray[i]);
-                fileStreamWriter.Close();
-            }
-
-            return;
-        }
-
-        public static void MainFW(string[] userData)
-        {
-            string root = Path.GetPathRoot(Directory.GetCurrentDirectory());
-            string dir = (root + @"NeaFiles\");
-
-            bool dirCheck = Directory.Exists(dir + userData[4] + @"\");
-
-            if (dirCheck == true)
-            {
-                Console.WriteLine("User account already exists");
-                return; //Change when user login is created
-            }
-
-            Directory.CreateDirectory(dir + userData[4] + @"\");
-            FileWriter(dir, userData);
-            return;
-        }
-
-        public static bool DirCheck(string dir)
-        {
-            bool check = File.Exists(dir);
-            return check;
-        }
-
-        public static string GetUserData(string dir)
-        {
-            if (DirCheck(dir) == false)
-            {
-                string e = "y";
-                return e;
-            }
-            else
-            {
-                string userName;
-                string userNameLocation = dir;
-                StreamReader ReadUserName = new StreamReader(dir);
-                userName = Convert.ToString(ReadUserName.ReadLine());
-                ReadUserName.Close();
-                return userName;
-            }
-        }
-    }
-    class Login
-    {
-        public static string SignIn()
-        {
-            string ActualUserName;
-            bool userLoop = false;
-
-            do
-            {
-                Console.WriteLine("Please enter your username");
-                string InputUserName = Console.ReadLine();
-                InputUserName = InputUserName + @"\Username.txt";
-
-                string UserDir = FileWrite.GetUserDir(InputUserName);
-                ActualUserName = FileWrite.GetUserData(UserDir);
-
-                if (ActualUserName == "y")
-                {
-                    Console.WriteLine("User account not found");
-                    Console.WriteLine(" ");
-                    userLoop = true;
-                }
-            } while (userLoop == true);
-
-            Console.WriteLine("Please enter your password");
-            string InputPassword = Console.ReadLine();
-            InputPassword = InputPassword + @"\Password.txt";
-
-            string PasswordDir = FileWrite.GetUserDir(InputPassword);
-            string ActualPassword = FileWrite.GetUserData(PasswordDir);
-
-            Console.ReadKey();
-
-            return ActualUserName;
         }
     }
 }
