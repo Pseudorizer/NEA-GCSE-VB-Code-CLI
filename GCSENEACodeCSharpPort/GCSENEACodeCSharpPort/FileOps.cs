24e714bf9c63be098e7b1fd5ecd546a9b398f3aa
MiniCheese26 <kaleb.medhurst@gmail.com>
2018-02-18 17:28:42 +0000

    Finished login system

---
+++ GCSENEACodeCSharpPort/GCSENEACodeCSharpPort/FileOps.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+
+namespace GCSENEACodeCSharpPort
+{
+    class FileOps : Start
+    {
+        public static string GetUserDir(string inputUserName)
+        {
+            string root = Path.GetPathRoot(Directory.GetCurrentDirectory());
+            string dir = root + @"NeaFiles\" + inputUserName;
+            string i = GetUserData(dir);
+
+            return i;
+        }
+
+        public static void FileWriter(string dir, string[] userDataArray)
+        {
+            string[] fileNames = new string[5] { "Name", "Password", "Age", "Year Group", "Username" };
+            string userName = userDataArray[4];
+
+            FileStream fileCreate;
+
+            userName = userName + @"\";
+
+            for (int i = 0; i < 5; i++)
+            {
+                string p = fileNames[i] + ".txt";
+                fileCreate = File.Create(dir + userName + p);
+                fileCreate.Close();
+
+                StreamWriter fileStreamWriter = new StreamWriter(dir + userName + p);
+
+                fileStreamWriter.WriteLine(userDataArray[i]);
+                fileStreamWriter.Close();
+            }
+
+            return;
+        }
+
+        public static void MainFW(string[] userData)
+        {
+            string root = Path.GetPathRoot(Directory.GetCurrentDirectory());
+            string dir = (root + @"NeaFiles\");
+
+            bool dirCheck = Directory.Exists(dir + userData[4] + @"\");
+
+            if (dirCheck == true)
+            {
+                Console.WriteLine("User account already exists");
+                Console.WriteLine(" ");
+                Login.SignIn();
+            }
+
+            Directory.CreateDirectory(dir + userData[4] + @"\");
+            FileWriter(dir, userData);
+            return;
+        }
+
+        public static bool DirCheck(string dir)
+        {
+            bool check = File.Exists(dir);
+            return check;
+        }
+
+        public static string GetUserData(string dir)
+        {
+            if (DirCheck(dir) == false)
+            {
+                string e = "y";
+                return e;
+            }
+            else
+            {
+                string userName;
+                string userNameLocation = dir;
+                StreamReader ReadUserName = new StreamReader(dir);
+                userName = Convert.ToString(ReadUserName.ReadLine());
+                ReadUserName.Close();
+                return userName;
+            }
+        }
+    }
+}
---
+++ GCSENEACodeCSharpPort/GCSENEACodeCSharpPort/Login.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace GCSENEACodeCSharpPort
+{
+    class Login
+    {
+        public static void SignIn()
+        {
+            string userName;
+            string PassInput;
+            bool UserAccountCheck = false;
+            bool PasswordComparisonCheck;
+
+            do
+            {
+                Console.WriteLine("Please enter your username");
+                userName = Console.ReadLine();
+                string userDir = userName + @"\Username.txt";
+
+                string UserCheck = FileOps.GetUserDir(userDir);
+
+                if (UserCheck == "y")
+                {
+                    Console.WriteLine("User account not found");
+                    UserAccountCheck = true;
+                }
+                else
+                {
+                    UserAccountCheck = false;
+                }
+            } while (UserAccountCheck == true);
+
+            do
+            {
+                Console.WriteLine("Please enter your password");
+                PassInput = Console.ReadLine();
+                string passDir = userName + @"\Password.txt";
+
+                string passRead = FileOps.GetUserDir(passDir);
+                passRead.Trim();
+
+                PasswordComparisonCheck = PasswordCheck(PassInput, passRead);
+            } while (PasswordComparisonCheck == false);
+
+            return;
+        }
+
+        static bool PasswordCheck(string userInput, string realPassword)
+        {
+            bool c = string.Equals(userInput, realPassword);
+            switch (c)
+            {
+                case true:
+                    Console.WriteLine("Password matches");
+                    return true;
+                case false:
+                    Console.WriteLine("Password incorrect");
+                    return false;
+            }
+            return false;
+        }
+    }
+}
